3543a1ee3b2c4fcaf9aba4d6ddac9a4390c37e0b
cormeistro <coreycollinsm@gmail.com>
2023-05-18 17:52:37 -0400

    Initial Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./styles/App.css";
+import Logo from "./assets/MWM.svg";
+import { BsFacebook, BsInstagram } from "react-icons/bs";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+    <div className="App">
+      <img src={Logo} alt="MWM Logo" />
+      <h1>Michigan Whiteout Media</h1>
+      <h2>West-Michigan's Premiere Action Photographers</h2>
+      <p>
+        This website is currently under construction. Please connect with us via
+        social media for immediate responses.
       </p>
-    </>
-  )
+      <div className="social-links">
+        <a
+          href="http://instagram.com/michiganwhiteout"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <div className="icon">
+            <BsFacebook />
+          </div>
+        </a>
+        <a
+          href="http://instagram.com/michiganwhiteout"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <div className="icon">
+            <BsInstagram />
+          </div>
+        </a>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
